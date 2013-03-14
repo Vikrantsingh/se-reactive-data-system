a1266c691fe484a6a5b3fa0a63f5f75c9e10a25d
pridhvi.bittu@gmail.com <pridhvi.bittu@gmail.com@f04bc015-e8db-9cd4-1910-fa2508c8f68c>
2013-03-14 13:36:01 +0000

    with registration

--- eda_event_repo.sql
+++ eda_event_repo.sql
@@ -1,47 +1,37 @@
--- phpMyAdmin SQL Dump
--- version 3.4.9
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Mar 08, 2013 at 02:52 PM
--- Server version: 5.5.25
--- PHP Version: 5.3.9
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `eda_event_repo`
---
-
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.5.15 - MySQL Community Server (GPL)
+-- Server OS:                    Win32
+-- HeidiSQL version:             7.0.0.4053
+-- Date/time:                    2013-03-14 19:08:04
 -- --------------------------------------------------------
 
---
--- Table structure for table `login`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
 
-CREATE TABLE IF NOT EXISTS `login` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+-- Dumping database structure for eda_event_repo
+CREATE DATABASE IF NOT EXISTS `eda_event_repo` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `eda_event_repo`;
+
+
+-- Dumping structure for table eda_event_repo.temp_login
+CREATE TABLE IF NOT EXISTS `temp_login` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(50) DEFAULT NULL,
   `password` varchar(50) DEFAULT NULL,
   `active` varchar(50) DEFAULT 'false',
-  `status` varchar(50) DEFAULT 'new',
+  `status` varchar(50) NOT NULL DEFAULT 'new',
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `login`
---
-
-INSERT INTO `login` (`id`, `username`, `password`, `active`, `status`) VALUES
-(1, 'ABC', 'XYZ', 'true', 'verified');
-
+-- Dumping data for table eda_event_repo.temp_login: ~3 rows (approximately)
+/*!40000 ALTER TABLE `temp_login` DISABLE KEYS */;
+INSERT INTO `temp_login` (`id`, `username`, `password`, `active`, `status`, `time`) VALUES
+	(9, 'pridhvi.kodamasimham@iiitb.org', 'emi', 'false', 'new', '0000-00-00 00:00:00'),
+	(10, 'pridhvi.bittu@gmail.com', 'emi', 'false', 'verified', '0000-00-00 00:00:00'),
+	(11, 'simaham007@gmail.com', 'emi', 'false', 'verified', '0000-00-00 00:00:00');
+/*!40000 ALTER TABLE `temp_login` ENABLE KEYS */;
+/*!40014 SET FOREIGN_KEY_CHECKS=1 */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
